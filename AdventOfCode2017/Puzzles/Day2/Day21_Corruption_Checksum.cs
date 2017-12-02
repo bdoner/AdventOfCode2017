0a82263d72991eb12b1c29fd9533752ac4a9a12a
bdoner <bdoner@noreply.user.github.com>
2017-12-02 13:41:11 +0100

    Day 2 complete

---
+++ AdventOfCode2017/Puzzles/Day2/Day21_Corruption_Checksum.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System;
+using System.Linq;
+
+namespace AdventOfCode2017.Puzzles.Day1
+{
+    public class Day21_Corruption_Checksum : IPuzzle
+    {
+        public string Run()
+        {
+            var input = 
+                File.ReadAllLines("Puzzles\\Day2\\input.txt")
+                .Select(line => 
+                    line.Split("\t", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                )
+                .ToArray();
+
+            int sum = 0;
+            foreach(var line in input)
+            {
+                var min = line.Min();
+                var max = line.Max();
+
+                var diff = max - min;
+                sum += diff;
+            }
+
+            
+            return sum.ToString();
+        }
+    }
+}
---
+++ AdventOfCode2017/Puzzles/Day2/Day22_Corruption_Checksum.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Puzzles.Day1
+{
+    public class Day22_Corruption_Checksum : IPuzzle
+    {
+        public string Run()
+        {
+            var input = 
+                File.ReadAllLines("Puzzles\\Day2\\input.txt")
+                .Select(line => 
+                    line.Split("\t", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()
+                )
+                .ToArray();
+
+            int sum = 0;
+            foreach(var line in input)
+            {
+                for(var i = 0; i < line.Count(); i++)
+                {
+                    for(var j = 0; j < line.Count(); j++)
+                    {
+                        if(i == j) continue;
+
+                        if(line[i] % line[j] == 0) 
+                        {
+                            sum += line[i] / line[j];
+                        }
+                    }
+                }
+            }
+            
+            return sum.ToString();
+        }
+    }
+}
