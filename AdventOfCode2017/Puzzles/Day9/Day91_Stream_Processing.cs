50059a51f856f49aa7476637bddee0340619531d
bdoner <bdoner@noreply.user.github.com>
2017-12-09 19:57:42 +0100

    Day 9 pt. 1 and 2 complete

---
+++ AdventOfCode2017/Puzzles/Day9/Day91_Stream_Processing.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2017.Puzzles.Day9
+{
+    public class Day91_Stream_Processing : IPuzzle
+    {
+        public string Run()
+        {
+            var stream =
+                File.ReadAllText("Puzzles\\Day9\\input.txt")
+                .ToCharArray();
+
+            var groups = new List<int>();
+
+            bool readingGarbage = false;
+            int openGroups = 0;
+
+            for (var i = 0; i < stream.Length; i++)
+            {
+                char c = stream[i];
+                if (c == '!')
+                {
+                    i++;
+                    continue;
+                }
+
+                if (readingGarbage && c == '>')
+                {
+                    readingGarbage = false;
+                    continue;
+                }
+                else if(readingGarbage)
+                {
+                    continue;
+                }
+
+                if(!readingGarbage && c == '<')
+                {
+                    readingGarbage = true;
+                    continue;
+                }
+
+                if (c == '{')
+                {
+                    openGroups++;
+                }
+                else if(c == '}')
+                {
+                    groups.Add(openGroups--);
+                }
+                
+            }
+
+            return groups.Sum().ToString();
+        }
+    }
+}
